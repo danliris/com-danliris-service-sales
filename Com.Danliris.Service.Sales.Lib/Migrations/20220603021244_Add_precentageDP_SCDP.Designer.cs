83e92fc65d14e7c77b7510010f750a7238837cfd
RichardoKirana <76505844+RichardoKirana@users.noreply.github.com>
2022-06-03 14:10:58 +0700

    Enhance SC DP

--- Com.Danliris.Service.Sales.Lib/Migrations/20220603021244_Add_precentageDP_SCDP.Designer.cs
+++ Com.Danliris.Service.Sales.Lib/Migrations/20220603021244_Add_precentageDP_SCDP.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Sales.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Sales.Lib.Migrations
 {
     [DbContext(typeof(SalesDbContext))]
-    partial class SalesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220603021244_Add_precentageDP_SCDP")]
+    partial class Add_precentageDP_SCDP
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
